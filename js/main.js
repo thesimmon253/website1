d360a4fca3430c3673ca8637ee730693fa76c6af
thesimmon253 <131204194+thesimmon253@users.noreply.github.com>
2023-07-24 20:22:45 -0500

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function suma(){
+    let A = 0;
+    let B = 0;
+
+    A = parseInt("Por favor ingrese el primer valor");
+    B = parseInt("Por favor ingrese el segundo valor");
+
+    SUMA = A + B;
+
+    alert ("La suma es igual a:" + SUMA);
+  
+}
